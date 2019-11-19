99dc5b80e726764c7329a9eecf7c19ebd7576433
jaeilkim <w8230@naver.com>
2019-11-19 13:30:42 +0900

    임수정

--- src/main/java/mes/sensorview/mesScm/InOut/InOutRestController.java
+++ src/main/java/mes/sensorview/mesScm/InOut/InOutRestController.java
@@ -1,7 +1,22 @@
 package mes.sensorview.mesScm.InOut;
 
+import mes.sensorview.Common.DataTransferObject.Message;
+import mes.sensorview.mesScm.InOut.DTO.SCM_IN;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class InOutRestController {
+
+    @Autowired
+    private InOutService inOutService;
+
+    @RequestMapping(value = "/scmInAdd", method = RequestMethod.POST)
+    public Message scmInAdd(HttpServletRequest req, SCM_IN scmIn) {
+        return inOutService.scmInAdd(req, scmIn);
+    }
 }
