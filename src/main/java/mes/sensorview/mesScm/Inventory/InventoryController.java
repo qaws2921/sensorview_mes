66078772623d23b4c8c66388fa9ef586b2bcf8bd
harikku <harikku@naver.com>
2019-11-21 17:04:12 +0900

    자재관리-재고관리 페이지

--- src/main/java/mes/sensorview/mesScm/Inventory/InventoryController.java
+++ src/main/java/mes/sensorview/mesScm/Inventory/InventoryController.java
@@ -1,7 +1,18 @@
 package mes.sensorview.mesScm.Inventory;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InventoryController {
+    @RequestMapping(value = "/scmStockList")
+    public String sysBPartGroup() {
+        return "mesScm/Inventory/scmStockList/scmStockList";
+    }
+
+    @RequestMapping(value ="/scmStockSumDay")
+    public String scmStockSumDay() { return "mesScm/Inventory/scmStockSumDay/scmStockSumDay"; }
+
+    @RequestMapping(value ="/scmStockSumMonth")
+    public String scmStockSumMonth() { return "mesScm/Inventory/scmStockSumMonth/scmStockSumMonth"; }
 }
