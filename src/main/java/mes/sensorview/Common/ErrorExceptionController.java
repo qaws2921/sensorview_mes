764ed6753432a0379c1b8d67bb58dc4724187dba
jaeilkim <w8230@naver.com>
2019-12-02 08:33:34 +0900

    에러페이지 처리

--- src/main/java/mes/sensorview/Common/ErrorExceptionController.java
+++ src/main/java/mes/sensorview/Common/ErrorExceptionController.java
@@ -4,11 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
@@ -17,13 +16,20 @@ public class ErrorExceptionController implements ErrorController {
 
     private static final String ERROR_PATH = "/error";
 
+    public String getDate(){
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_ss");
+        String date = format.format(now);
+        return date;
+    }
+
     @Override
     public String getErrorPath() {
         return ERROR_PATH;
     }
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
+    public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
 
@@ -32,7 +38,7 @@ public class ErrorExceptionController implements ErrorController {
             log.info("########## ErrorExceptionController ##########");
             log.info("에러 구분  : " + status.toString());
             log.info("에러 메세지 : " + httpStatus.getReasonPhrase());
-            log.info("발생일     : " +  new Date());
+            log.info("발생일     : " +  getDate());
             log.info("##############################################");
             return "error/404";
         }
@@ -41,7 +47,7 @@ public class ErrorExceptionController implements ErrorController {
             log.info("########## ErrorExceptionController ##########");
             log.info("에러 구분  : " + status.toString());
             log.info("에러 메세지 : " + httpStatus.getReasonPhrase());
-            log.info("발생일     : " +  new Date());
+            log.info("발생일     : " +  getDate());
             log.info("##############################################");
             return "error/500";
         }
