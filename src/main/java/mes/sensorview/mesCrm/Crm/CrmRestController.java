2d551fa61c9ee51e9130ecf9a0811eae200fb50a
jaeilkim <w8230@naver.com>
2019-12-02 13:49:54 +0900

    에러페이지 처리

--- src/main/java/mes/sensorview/mesCrm/Crm/CrmRestController.java
+++ src/main/java/mes/sensorview/mesCrm/Crm/CrmRestController.java
@@ -1,7 +1,21 @@
 package mes.sensorview.mesCrm.Crm;
 
+import mes.sensorview.Common.DataTransferObject.Message;
+import mes.sensorview.mesCrm.Crm.DTO.CRM_ORD_RECP;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class CrmRestController {
+    @Autowired
+    private CrmService crmService;
+
+    @RequestMapping(value = "/crmRecpAdd", method = RequestMethod.POST)
+    public Message crmRecpAdd(HttpServletRequest req, CRM_ORD_RECP crmOrdRecp) {
+        return crmService.crmRecpAdd(req, crmOrdRecp);
+    }
 }
--- src/main/java/mes/sensorview/mesCrm/Crm/CrmService.java
+++ src/main/java/mes/sensorview/mesCrm/Crm/CrmService.java
@@ -1,7 +1,22 @@
 package mes.sensorview.mesCrm.Crm;
 
+import mes.sensorview.Common.DataTransferObject.Message;
+import mes.sensorview.Common.Function.ReturnFunction;
+import mes.sensorview.Mapper.mesCrm.CRM.CrmMapper;
+import mes.sensorview.mesCrm.Crm.DTO.CRM_ORD_RECP;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Service
-public class CrmService {
+public class CrmService extends ReturnFunction {
+    @Autowired
+    private CrmMapper crmMapper;
+
+    public Message crmRecpAdd(HttpServletRequest req, CRM_ORD_RECP crmOrdRecp) {
+        crmOrdRecp.setSite_code(getSessionData(req).getSite_code());
+        crmOrdRecp.setUser_code(getSessionData(req).getUser_code());
+        return crmMapper.crmRecpAdd(crmOrdRecp);
+    }
 }
