2dc24257f9e8180398308cd2ca7252524ffbed39
harikku <harikku@naver.com>
2020-01-02 11:28:32 +0900

    바코드양식

--- src/main/java/mes/sensorview/Mapper/mesPop/Standard/MesPopMapper.java
+++ src/main/java/mes/sensorview/Mapper/mesPop/Standard/MesPopMapper.java
@@ -1,4 +1,15 @@
 package mes.sensorview.Mapper.mesPop.Standard;
 
+import mes.sensorview.Common.DataTransferObject.Message;
+import mes.sensorview.Common.DataTransferObject.Page;
+import mes.sensorview.mesPop.Standard.DTO.POP_BCR_FORM;
+
+import java.util.List;
+
 public interface MesPopMapper {
+    List<POP_BCR_FORM> popBcrFormGet(Page p);
+
+    POP_BCR_FORM popBcrFormOneGet(POP_BCR_FORM pbf);
+
+    Message popBcrFormAdd(POP_BCR_FORM pbf);
 }
