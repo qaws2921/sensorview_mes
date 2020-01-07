2102fb9ec785422179a216c334a8273e65c47b54
harikku <harikku@naver.com>
2020-01-07 09:18:01 +0900

    자재출고현황

--- src/main/resources/static/data-component/mesSCM/InOut/scmOutList/scmOutList.js
+++ src/main/resources/static/data-component/mesSCM/InOut/scmOutList/scmOutList.js
@@ -5,7 +5,7 @@
 ////////////////////////////데이터/////////////////////////////////////
 
 var main_data = {
-    send_data: {},
+    send_data: {}
 };
 
 ////////////////////////////시작 함수/////////////////////////////////////
@@ -41,19 +41,18 @@ function jqGrid_main() {
     $('#mes_grid').jqGrid({
         datatype: "local",
         mtype: 'POST',
-        colNames: ['출고일자', '출고번호', '공정명', '품목그룹', '품번', '품명', '규격', '단위', '출고수량', '등록자', '출고일시'],
+        colNames: ['출고일자', '출고번호', '공정명', '품번', '품명', '규격', '단위', '출고수량', '등록자', '출고일시'],
         colModel: [
             {name: 'work_date', index: 'work_date', sortable: false, width: 60, formatter: formmatterDate2},
             {name: 'out_no', index: 'out_no', sortable: false, width: 60},
             {name: 'line_name', index: 'line_name', sortable: false, width: 40},
-            {name: 'part_grp_name', index: 'part_grp_name', sortable: false, width: 60},
             {name: 'part_code', index: 'part_code', sortable: false, width: 60},
             {name: 'part_name', index: 'part_name', sortable: false, width: 60},
             {name: 'spec', index: 'spec', sortable: false, width: 60},
             {name: 'unit_name', index: 'unit_name', sortable: false, width: 40},
             {name: 'qty', index: 'qty', sortable: false, width: 60},
             {name: 'user_name', index: 'user_name', sortable: false, width: 60},
-            {name: 'update_date', index: 'update_date', sortable: false, width: 80, formatter: formmatterDate},
+            {name: 'update_date', index: 'update_date', sortable: false, width: 80, formatter: formmatterDate}
         ],
         caption: "출고현황 | MES",
         autowidth: true,
@@ -61,7 +60,7 @@ function jqGrid_main() {
         pager: '#mes_grid_pager',
         rowList: [100, 200, 300, 500, 1000],
         rowNum: 100,
-        viewrecords: true,
+        viewrecords: true
 
     });
 }
