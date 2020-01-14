a5ede07e08296a5c4494d348c00e8302d9ebc1de
jonghyo <whdgy7@naver.com>
2020-01-14 11:20:21 +0900

    발주등록 모달 수정

--- src/main/resources/static/data-component/mesCRM/Crm/crmAssyCable/crmAssyCable_modal1.js
+++ src/main/resources/static/data-component/mesCRM/Crm/crmAssyCable/crmAssyCable_modal1.js
@@ -80,11 +80,24 @@ function modal_make1() {
         height: 'auto',
         autoOpen:false,
         resizable: false,
-        buttons: [
-            {
-                "class": "hide",
+        open: function () {
+            if ($.ui && $.ui.dialog && !$.ui.dialog.prototype._allowInteractionRemapped && $(this).closest(".ui-dialog").length) {
+                if ($.ui.dialog.prototype._allowInteraction) {
+                    $.ui.dialog.prototype._allowInteraction = function (e) {
+                        if ($(e.target).closest('.select2-drop').length) return true;
+
+                        if (typeof ui_dialog_interaction != "undefined") {
+                            return ui_dialog_interaction.apply(this, arguments);
+                        } else {
+                            return true;
+                        }
+                    };
+                    $.ui.dialog.prototype._allowInteractionRemapped = true;
+                } else {
+                    $.error("You must upgrade jQuery UI or else.");
+                }
             }
-        ]
+        }
     });
 }
 
