4f62ae03b9af15005930c254fa73e7dba7ce56c6
jonghyo <whdgy7@naver.com>
2020-02-04 15:15:41 +0900

    자재등록 수정

--- src/main/resources/static/data-component/admin/bom/sysPart/sysPart.js
+++ src/main/resources/static/data-component/admin/bom/sysPart/sysPart.js
@@ -9,62 +9,46 @@ var main_data = {
     send_data: {},
     send_data_post: {},
     readonly: [],
-    auth:{}
+    auth:{},
+    condition_data:{}
 };
 
-var colNames = ['구분','t1','t2','t3','품목코드','품목명','보관로케이션','업체명','규격','단위','L/T','검사등급','재고최대','재고최소','등록자','수정일']
 
 ////////////////////////////시작 함수//////////////////////////////////
 $(document).ready(function () {
     jqGrid_main();
     jqGridResize('#mes_grid', $('#mes_grid').closest('[class*="col-"]'));
-    jqgridPagerIcons();
 
     selectBox();
     authcheck();
     modal_start1();
+    jqgridPagerIcons();
 });
 
 ////////////////////////////클릭 함수//////////////////////////////////
-function select_change1(value) {
-    ccn_ajax('/sysPartTypeOneGet',{keyword:'',keyword2:value}).then(function (value) {
-        for(var i=1; i<=3;i++) {
-            group_cb(value,i);
-            group_cb_modal1(value,i);
-        }
-        grid_head_value_change(value);
-    });
-}
+
 
 function add_btn() {
     if (main_data.auth.check_add !="N") {
-        modal_reset(".modal_value", main_data.readonly);
-        $('#part_group1_modal1').text($('#part_group1').text());
-        $('#part_group2_modal1').text($('#part_group2').text());
-        $('#part_group3_modal1').text($('#part_group3').text());
 
-        modalValuePush("#part_type_select","#part_type_code","#part_type_name");
-        main_data.check = 'I';
-        if($('#part_group_select1').val() == ''){
-            $("select[name=part_group1] option:eq(0)").prop("selected", true).trigger("change");
-        }else {
-            $('#part_group_select1_modal1').val($('#part_group_select1').val()).trigger("change");
-        }
-        if($('#part_group_select2').val() == ''){
-            $("select[name=part_group2] option:eq(0)").prop("selected", true).trigger("change");
-        }else {
-            $('#part_group_select2_modal1').val($('#part_group_select2').val()).trigger("change");
-        }
-        if($('#part_group_select3').val() == ''){
-            $("select[name=part_group3] option:eq(0)").prop("selected", true).trigger("change");
-        }else {
-            $('#part_group_select3_modal1').val($('#part_group_select3').val()).trigger("change");
-        }
-        $("select[name=unit_name] option:eq(0)").prop("selected", true).trigger("change");
-        $("select[name=qc_level] option:eq(0)").prop("selected", true).trigger("change");
-        $("select[name=loc_code] option:eq(0)").prop("selected", true).trigger("change");
+        main_data.condition_data = value_return(".condition_main");
 
-        $("#addDialog").dialog('open');
+        if (effectiveness_main(main_data.condition_data)) {
+            modal_reset(".modal_value", main_data.readonly);
+            $("#part_name").val(main_data.condition_data.keyword4);
+            $("select[name=prod_type] option:eq(0)").prop("selected", true).trigger("change");
+            $("select[name=material_type] option:eq(0)").prop("selected", true).trigger("change");
+            $("select[name=loc_code] option:eq(0)").prop("selected", true).trigger("change");
+            $("select[name=unit_code] option:eq(0)").prop("selected", true).trigger("change");
+            $("select[name=qc_level] option:eq(0)").prop("selected", true).trigger("change");
+
+            main_data.check = 'I';
+
+
+            $("#addDialog").dialog('open');
+        }
+
+
     } else {
         alert("추가권한이 없습니다,");
     }
@@ -72,9 +56,8 @@ function add_btn() {
 
 function get_btn(page) {
     main_data.send_data = value_return(".condition_main");
+    main_data.send_data.keyword = 'D';
     main_data.send_data_post = main_data.send_data;
-
-    grid_head_change();
 
     $("#mes_grid").setGridParam({
         url: '/sysPartGet',
@@ -98,42 +81,15 @@ function update_btn(jqgrid_data) {
         modal_reset(".modal_value", []);
         main_data.check = 'U';
         ccn_ajax('/sysPartOneGet', {keyword:jqgrid_data.part_code}).then(function (data) {
-            return data;
-        }).then(function (data2) {
-            for (var i = 1; i<=3; i++){
-                selectBoxPartModal(data2,i)
-            }
-            return data2;
-        }).then(function (data5) {
-            ccn_ajax('/sysPartTypeOneGet',{keyword:'',keyword2:data5.part_type}).then(function (value) {
-                $('#part_group1_modal1').text(value.part_group1);
-                $('#part_group2_modal1').text(value.part_group2);
-                $('#part_group3_modal1').text(value.part_group3);
-            });
-            // data5.qc_level=data5.qc_level.replace(/(\s*)/g,"");
-            modal_edits('.modal_value', main_data.readonly,data5); // response 값 출력
+            modal_edits('.modal_value', main_data.readonly,data); // response 값 출력
             $("#addDialog").dialog('open');
-
         });
     } else {
         alert("수정권한이 없습니다.");
     }
 }
 
-function selectBoxPartModal(value,i) {
 
-    ccn_ajax('/sysPartGroupAllGet',{keyword:value.part_type,keyword2:i}).then(function (value1) {
-        $('#part_group_select'+i+'_modal1').empty();
-        var option = null;
-        for(var j=0;j<value1.length;j++){
-            option = $("<option></option>").text(value1[j].part_grp_name).val(value1[j].part_grp_code);
-            $('#part_group_select'+i+'_modal1').append(option);
-        }
-        $('#part_group_select'+i+'_modal1').select2();
-        $('#part_group_select'+i+'_modal1').val(value['part_group'+i]).trigger("change");
-    });
-
-}
 
 
 
@@ -167,6 +123,11 @@ function delete_btn() {
 }
 
 
+function select_change1(value) {
+    select_makes3('#part_group2_select', "/sysPartGroup2AllGet","part_grp_code2" ,"part_grp_name2",{keyword:'D', keyword2:value});
+}
+
+
 ////////////////////////////호출 함수//////////////////////////////////
 function authcheck() {
     ccn_ajax("/menuAuthGet", {keyword: "sysPart"}).then(function (data) {
@@ -174,56 +135,14 @@ function authcheck() {
     });
 }
 
-function grid_head_value_change(value) {
-    colNames[1] = value.part_group1;
-    colNames[2] = value.part_group2;
-    colNames[3] = value.part_group3;
-}
 
-function grid_head_change() {
-    $.jgrid.gridUnload('#mes_grid');
-    jqGrid_main();
-    jqGridResize2('#mes_grid', $('#mes_grid').closest('[class*="col-"]'));
-    jqgridPagerIcons();
-}
-
-function group_cb(value,i) {
-    $('#part_group'+i).text(value["part_group"+i]);
-    ccn_ajax('/sysPartGroupAllGet',{keyword:value.part_type_code,keyword2:i}).then(function (value1) {
-        $('#part_group_select'+i).empty();
-        var option = null;
-        var allSelect = ($("<option></option>").text("전체").val(""));
-        $('#part_group_select'+i).append(allSelect);
-        for(var j=0;j<value1.length;j++){
-            option = $("<option></option>").text(value1[j].part_grp_name).val(value1[j].part_grp_code);
-            $('#part_group_select'+i).append(option);
-        }
-        $('#part_group_select'+i).select2();
-    });
-}
-
-function group_cb_modal1(value,i) {
-    ccn_ajax('/sysPartGroupAllGet',{keyword:value.part_type_code,keyword2:i}).then(function (value1) {
-        $('#part_group_select'+i+'_modal1').empty();
-        var option = null;
-        for(var j=0;j<value1.length;j++){
-            option = $("<option></option>").text(value1[j].part_grp_name).val(value1[j].part_grp_code);
-            $('#part_group_select'+i+'_modal1').append(option);
-        }
-        $('#part_group_select'+i+'_modal1').select2();
-    });
-}
 function selectBox() {
-    part_type_select_ajax("#part_type_select", "/sysPartTypeGet", "part_type_code", "part_type_name",{keyword:''}).then(function (data) {
-        ccn_ajax('/sysPartTypeOneGet',{keyword:'',keyword2:data[0].part_type_code}).then(function (value) {
-            for(var i=1; i<=3;i++) {
-                group_cb(value,i);
-                group_cb_modal1(value,i);
-            }
-            grid_head_value_change(value);
-            grid_head_change();
-        })
+    part_type_select_ajax("#part_group1_select", "/sysPartGroupAllGet", "part_grp_code", "part_grp_name",{keyword:'D'}).then(function (data) {
+        select_makes3('#part_group2_select', "/sysPartGroup2AllGet","part_grp_code2" ,"part_grp_name2",{keyword:'D', keyword2:data[0].part_grp_code});
     });
+
+    select_makes_sub('#part_name_select', "/sysPartNameGroupAllGet","code_name2" ,"code_name2",{keyword:'MAT_PROD', keyword2:'CODE'},'Y');
+
 
 }
 
@@ -231,17 +150,20 @@ function jqGrid_main() {
     $('#mes_grid').jqGrid({
         datatype: "local",
         mtype: 'POST',
-        colNames: colNames,
+        colNames: ['품번','품명','규격1','규격2','재질','제조사','공급사','용도','자재유형','품목군','제품군','보관로케이션','단위','L/T','검사등급','재고최대','재고최소','등록자','수정일'],
         colModel: [
-            {name: 'part_type_name', index: 'part_type_name', sortable: false, width: 60},
-            {name: 'part_grp_name1', index: 'part_grp_name1', sortable: false, width: 60},
-            {name: 'part_grp_name2', index: 'part_grp_name2', sortable: false, width: 60},
-            {name: 'part_grp_name3', index: 'part_grp_name3', sortable: false, width: 60},
             {name: 'part_code', index: 'part_code', key:true, sortable: false, width: 60},
             {name: 'part_name', index: 'part_name', sortable: false, width: 60},
-            {name: 'loc_name', index: 'loc_name', sortable: false, width: 60},
+            {name: 'spec1', index: 'spec1', sortable: false, width: 60},
+            {name: 'spec2', index: 'spec2', sortable: false, width: 60},
+            {name: 'material', index: 'material', sortable: false, width: 60},
+            {name: 'maker_name', index: 'maker_name', sortable: false, width: 60},
             {name: 'supp_name', index: 'supp_name', sortable: false, width: 60},
-            {name: 'spec', index: 'spec', sortable: false, width: 60},
+            {name: 'prod_type_name', index: 'prod_type_name', sortable: false, width: 60},
+            {name: 'material_type_name', index: 'material_type_name', sortable: false, width: 60},
+            {name: 'part_grp_name1', index: 'part_grp_name1', sortable: false, width: 60},
+            {name: 'part_grp_name2', index: 'part_grp_name2', sortable: false, width: 60},
+            {name: 'loc_name', index: 'loc_name', sortable: false, width: 60},
             {name: 'unit_name', index: 'unit_name', sortable: false, width: 60},
             {name: 'lt', index: 'lt', sortable: false, width: 60},
             {name: 'qc_level_name', index: 'qc_level_name', sortable: false, width: 60},
@@ -269,4 +191,20 @@ function jqGrid_main() {
             update_btn(data);
         }
     }).navGrid('#mes_grid_pager', {search: false, add: false, edit: false, del: false});
+}
+
+function effectiveness_main(data) {
+    if (data.keyword2 === "" || data.keyword2 === null){
+        alert("품목군을 다시 선택해주세요");
+        return false;
+    } else if (data.keyword3 === "" || data.keyword3 === null){
+        alert("제품군을 다시 선택해주세요");
+        return false;
+    } else if (data.keyword4 === "" || data.keyword4 === null){
+        alert("품명을 선택해주세요");
+        return false;
+    } else {
+        return  true;
+    }
+    
 }
